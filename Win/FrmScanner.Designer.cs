73032cb758721d96f09e66383934303e21bc4b83
Saemi hasan <hsaem92@gmail.com>
2019-02-27 17:06:02 +0330

    Add files via upload

---
+++ Win/FrmScanner.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Win
+{
+    partial class FrmScanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(22, 53);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(134, 23);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Start Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // FrmScanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 140);
+            this.Controls.Add(this.btnScan);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmScanner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnScan;
+    }
+}
---
+++ Win/FrmScanner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using asprise_imaging_api;
+
+namespace Win
+{
+    public partial class FrmScanner : Form
+    {
+        public FrmScanner()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            Result result = new AspriseImaging().Scan(new Request()
+                    .SetTwainCap(TwainConstants.ICAP_PIXELTYPE, TwainConstants.TWPT_RGB)
+                    .SetPromptScanMore(true) // prompt to scan more pages
+                    .AddOutputItem(new RequestOutputItem(AspriseImaging.OUTPUT_SAVE, AspriseImaging.FORMAT_JPG)
+                      .SetSavePath(".\\${TMS}${EXT}")), // Environment variables in path will be expanded
+                  "select", true, true); // "select" prompts device selection dialog.
+
+            List<string> files = result == null ? null : result.GetImageFiles();
+            MessageBox.Show("Scanned: " + string.Join(", ", files == null ? new string[0] : files.ToArray()));
+
+        }
+    }
+}
